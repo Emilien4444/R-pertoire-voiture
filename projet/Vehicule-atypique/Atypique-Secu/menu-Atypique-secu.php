884ba6bfd27d6633825ae566613cd18695b3b90d
Emilien4444 <131696305+Emilien4444@users.noreply.github.com>
2024-05-15 13:28:06 +0200

    Add files via upload

---
+++ projet/Vehicule-atypique/Atypique-Secu/menu-Atypique-secu.php
@@ -0,0 +1,11 @@
+<!-- Menu qui se trouve dans les pages des Véhicules Atypiques lorsqu'on est connecté -->
+
+<nav> 
+   <ul>
+		<li><a href="../../accueil-securise.php"> Accueil </a></li>
+		<li><a href="Vehicule-atypique-securise.php" >Véhicules atypiques</a></li>
+	  	<li><a href="Vehicule-atypique-AstonMartin-securise.php">Aston Martin </a></li>
+	  	<li><a href="Vehicule-atypique-KTM-securise.php"> KTM </a></li>
+		  <li><a href='../../favoris.php'>Vos favoris</a></li>;
+    </ul>
+</nav>
