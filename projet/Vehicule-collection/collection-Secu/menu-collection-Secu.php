2ea06783525a4e7fd97fc04b12bb93d2ca6f82dd
Emilien4444 <131696305+Emilien4444@users.noreply.github.com>
2024-05-15 21:18:47 +0200

    Add files via upload

---
+++ projet/Vehicule-collection/collection-Secu/menu-collection-Secu.php
@@ -0,0 +1,24 @@
+<!-- Menu de chaque page collection -->
+
+<nav> 
+    <ul>
+	  	<li><a href="../../accueil-securise.php"> Accueil </a></li>
+	  	<li><a href="Vehicule-collection-securise.php"> Véhicule de collection </a></li>
+        <li><a href="ancien-AstonMartin-Secu.php">Aston Martin </a></li>
+        <li> <a href="ancien-Audi-Secu.php">Audi </a></li>
+        <li> <a href="ancien-Bentley-Secu.php">Bentley </a></li>
+	  	<li> <a href="ancien-BMW-Secu.php">BMW </a></li>
+        <li> <a href="ancien-Bugatti-Secu.php">Bugatti </a></li>
+	  	<li> <a href="ancien-Cadillac-Secu.php">Cadillac </a></li>
+        <li><a href="ancien-Ferrari-Secu.php">Ferrari </a></li>
+        <li> <a href="ancien-Ford-Secu.php">Ford </a></li>
+        <li> <a href="ancien-Jaguard-Secu.php">Jaguard </a></li>
+        <li> <a href="ancien-Lamborghini-Secu.php">Lamborghini </a></li>
+	  	<li> <a href="ancien-Lincoln-Secu.php">Lincoln </a></li>
+        <li> <a href="ancien-Maserati-Secu.php">Maserati </a></li>
+        <li><a href="ancien-Mercedes-Secu.php">Mercedes-Benz </a></li>
+        <li><a href="ancien-Porsche-Secu.php">Porsche </a></li>
+        <li> <a href="ancien-Renault-Secu.php">Renault </a></li>
+        <li> <a href="ancien-RollsRoyce-Secu.php">Rolls Royce </a></li>
+    </ul>
+</nav>
