f48eeece5f7304ef2d368f529f32dc417ca006fd
Emilien4444 <131696305+Emilien4444@users.noreply.github.com>
2024-05-15 12:48:00 +0200

    Delete deconnexion.php

--- deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	session_unset();
-	session_destroy();
-	header("Location: accueil.php?deconnexion=1"); // redirige avec un paramètre de succès pour pouvoir afficher une alert en JavaScript
-	exit();
-?>
