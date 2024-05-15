2bad355b36b4199206f27d8aa5aaf088542a58c5
Emilien4444 <131696305+Emilien4444@users.noreply.github.com>
2024-05-15 13:27:32 +0200

    Add files via upload

---
+++ projet/Vehicule-atypique/Atypique-pasSecu/menu-atypique.php
@@ -0,0 +1,10 @@
+<!-- Menu de chaque page sportif -->
+
+<nav> 
+    <ul>
+	  <li><a href="../../accueil.php"> Accueil </a></li>
+	  <li><a href="Vehicule-atypique.php"> Vehicule Atypique </a></li>
+      <li><a href="Vehicule-atypique-AstonMartin.php">Aston Martin </a></li>
+	  <li><a href="Vehicule-atypique-KTM.php"> KTM </a></li>
+    </ul>
+</nav>
